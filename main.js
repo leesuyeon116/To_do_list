e2d2050e2474357cc4a1e8348cec6b563cda3aa1
leesuyeon116 <suyeon873088@naver.com>
2024-07-04 00:21:31 +0900

    아이템 삭제하기 하는 중

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ let taskList = [];
 addButton.addEventListener("click", addTask);
 
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent);
+    let task = { 
+        id:randomIDGenerate(),
+        taskContent: taskInput.value,
+        isComplete:false
+    }
+    taskList.push(task);
     render();
     console.log(taskList);
 }
@@ -13,14 +17,44 @@ function addTask() {
 function render() {
     let resultHTML = "";
     for(let i = 0; i < taskList.length; i++) {
-        resultHTML += `<div class="task">
-            <div>${taskList[i]}</div>
-            <div>
-                <button>Check</button>
-                <button>Delete</button>
-            </div>
-        </div>`;
+        if(taskList[i].isComplete == true) {
+            resultHTML += `<div class="task">
+                <div class="task-done">${taskList[i].taskContent}</div>
+                <div>
+                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                    <button onclick="deleteTask()">Delete</button>
+                </div>
+            </div>`;
+        } else {
+            resultHTML += `<div class="task">
+                <div>${taskList[i].taskContent}</div>
+                <div>
+                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
+                    <button onclick="deleteTask()">Delete</button>
+                </div>
+            </div>`;
+        }
     }
 
+
     document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete; // 현재 값의 반대 값을 넣어줌 (true는 false로, false는 true로)
+            break;
+        }
+    }
+    render();
+    console.log(taskList);
+}
+
+function deleteTask() {
+    console.log("삭제");
+}
+
+function randomIDGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
 }
