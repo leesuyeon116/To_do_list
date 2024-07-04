0bcb72ddc66565b1b2df813a81c5292072b3d663
leesuyeon116 <suyeon873088@naver.com>
2024-07-04 13:46:32 +0900

    슬라이딩 애니메이션 하는 중

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-button");
+let tabs = document.querySelectorAll(".task-tabs div");
 let taskList = [];
+let mode = "all";
+let filterList = [];
 addButton.addEventListener("click", addTask);
+
+for(let i = 1; i < tabs.length; i ++) {
+    tabs[i].addEventListener("click", function (event) {
+        filter(event);
+    });
+}
 
 function addTask() {
     let task = { 
@@ -15,27 +24,33 @@ function addTask() {
 }
 
 function render() {
+    let list = [];
+    if(mode === "all") {
+        list = taskList;
+    } else if(mode === "ongoing" || mode === "done") {
+        list = filterList;
+    }
+
     let resultHTML = "";
-    for(let i = 0; i < taskList.length; i++) {
-        if(taskList[i].isComplete == true) {
+    for(let i = 0; i < list.length; i++) {
+        if(list[i].isComplete == true) {
             resultHTML += `<div class="task">
-                <div class="task-done">${taskList[i].taskContent}</div>
+                <div class="task-done">${list[i].taskContent}</div>
                 <div class="button-box">
-                    <button onclick="toggleComplete('${taskList[i].id}')"><i class="fas fa-undo-alt"></i></button>
-                    <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
+                    <button onclick="toggleComplete('${list[i].id}')"><i class="fas fa-undo-alt"></i></button>
+                    <button onclick="deleteTask('${list[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
                 </div>
             </div>`;
         } else {
             resultHTML += `<div class="task">
-                <div>${taskList[i].taskContent}</div>
+                <div>${list[i].taskContent}</div>
                 <div class="button-box">
-                    <button onclick="toggleComplete('${taskList[i].id}')"><i class="fa-solid fa-check"></i></button>
-                    <button onclick="deleteTask('${taskList[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
+                    <button onclick="toggleComplete('${list[i].id}')"><i class="fa-solid fa-check"></i></button>
+                    <button onclick="deleteTask('${list[i].id}')"><i class="fa-regular fa-trash-can"></i></button>
                 </div>
             </div>`;
         }
     }
-
 
     document.getElementById("task-board").innerHTML = resultHTML;
 }
@@ -59,6 +74,29 @@ function deleteTask(id) {
         }
     }
     render();
+}
+
+function filter(event) {
+    mode = event.target.id;
+    filterList = [];
+    if(mode === "all") {   
+        render();
+    } else if(mode === "ongoing") {  
+        for(let i = 0; i < taskList.length; i++) {
+            if(taskList[i].isComplete === false) {
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+        console.log("진행 중", filterList);
+    } else if(mode === "done") {   
+        for(let i = 0; i < taskList.length; i++) {
+            if(taskList[i].isComplete === true) {
+                filterList.push(taskList[i]);
+            }
+        }
+        render();
+    }
 }
 
 function randomIDGenerate() {
